018e4053a800e48cb9f04caecaad3f0036a9cf43
ahbean11 <ahbean11@icloud.com>
2025-12-11 23:13:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
-python -c "from app import db, app; with app.app_context(): db.create_all(); print('Database initialized successfully')"
+python init_db.py

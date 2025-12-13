88c7f2e39e734fa78d0c1214b91ff88cb1c2ec26
ahbean11 <ahbean11@icloud.com>
2025-12-13 17:57:31 +0700

    Xoá thừa + Update index

--- check_db.py
+++
@@ -1,8 +0,0 @@
-from app import db, app
-
-with app.app_context():
-    print('Connected to DB:', db.engine.url)
-    print('Tables before create_all:', list(db.metadata.tables.keys()))
-    db.create_all()
-    print('Tables after create_all:', list(db.metadata.tables.keys()))
-    print("Database tables created successfully!")
